1347ee1042aa5808b3904ca7ba6894bfb66d03d9
Bogdan <bogdanbatog@gmail.com>
2019-05-21 11:08:00 -0400

    read mysql connection string from env too

--- config/database.php
+++ config/database.php
@@ -16,7 +16,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'pgsql'),
+    'default' => env('DB_CONNECTION', 'mysql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -44,11 +44,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => env('DB_HOST', $herokuDatabase['host'] ?? '127.0.0.1'),
+            'port' => env('DB_PORT', $herokuDatabase['port'] ?? '3306'),
+            'database' => env('DB_DATABASE', $herokuDatabase['path'] ?? 'forge'),
+            'username' => env('DB_USERNAME', $herokuDatabase['user'] ?? 'forge'),
+            'password' => env('DB_PASSWORD', $herokuDatabase['pass'] ?? ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
